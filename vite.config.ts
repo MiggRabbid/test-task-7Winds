a16890e12ad3d0014b68c325a238f9170378d508
MiggRabbid <mig26@inbox.ru>
2024-08-13 18:06:07 +0300

    MAIN: preparing for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/test-task-7Winds',
+    base: '/test-task-7Winds/',
     plugins: [react()],
     build: {
         outDir: 'dist',
